91110f0d38a646ebff258e369e96a6d8800e968d
Rafidr1 <mdrafid2580+1@gmail.com>
2025-12-04 16:10:23 +0600

    data.js

---
+++ data.js
@@ -0,0 +1,33 @@
+/**
+ * =================================================================
+ * ⚙️ CARNIVAL PAY - CONTROL PANEL
+ * =================================================================
+ * Developed by: Rafid Islam
+ * =================================================================
+ */
+
+const siteData = {
+    // ১. পেমেন্ট সেটিংস
+    discountAmount: 10, // ছাড়ের পরিমাণ
+    paymentLink: "https://shop.bkash.com/carnival-internet--wifi-hut-al/paymentlink", // অটোমেটিক পেমেন্ট লিংক
+    merchantNumber: "01511715464", // অ্যাপ (ম্যানুয়াল) পেমেন্টের নাম্বার
+
+    // ২. নোটিশ বোর্ড
+    noticeText: "আসসালামু আলাইকুম! ওয়েবসাইট লিংক বা বিকাশ অ্যাপ—যেকোনো মাধ্যমেই বিল পেমেন্ট করলে পাচ্ছেন ১০ টাকা নিশ্চিত ক্যাশ ডিসকাউন্ট।",
+    noticeType: "blue", // 'blue' (নীল) বা 'red' (লাল)
+
+    // ৩. যোগাযোগ ও প্রোফাইল
+    hotline: "09642-111213",
+    ownerName: "Mohammad Yunus",
+    ownerDesignation: "Carnival Internet & WiFi Hut",
+    
+    // সোশ্যাল লিংক
+    whatsappLink: "https://wa.me/8801712370705",
+    facebookLink: "https://www.facebook.com/share/17UFEqGQ1z/",
+
+    // ৪. ভিডিও লিংক (YouTube Embed)
+    videoLinkSystem: "https://www.youtube.com/embed/kq44fjE64Uw", 
+    videoManualSystem: "https://www.youtube.com/embed/MZEbieYtfH0"
+};
+
+
