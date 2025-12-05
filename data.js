f77f70cb3738310b357b381fa8fc0644157b55f2
Rafidr1 <mdrafid2580+1@gmail.com>
2025-12-05 13:13:46 +0600

    Update data.js

--- data.js
+++ data.js
@@ -24,7 +24,7 @@ const siteData = {
     facebookLink: "https://www.facebook.com/share/17UFEqGQ1z/",
 
     // ৪. ভিডিও লিংক
-    videoLinkSystem: "https://youtube.com/shorts/8n9cjJT0h4c?feature=share", 
+    videoLinkSystem: "https://youtube.com/shorts/8n9cjJT0h4c?si=DQivNGM1gWsEIA_O", 
     videoManualSystem: "https://www.youtube.com/embed/MZEbieYtfH0"
 };
 
